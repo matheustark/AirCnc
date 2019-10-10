9872eafbc93aaf00ce9ed6e7e83229a397f69cbb
MatheusGonçalves <matheusglmonteiro@gmail.com>
2019-10-10 16:59:14 -0300

    add AirCnC app

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://matheustark321:Winter123@@cluster0-n6stp.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+app.listen(3333) ;  
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+
+import Routes from './routes';
+
+import Logo from './assets/logo.svg'
+
+function App() {
+ 
+
+  return (
+    <div className="container">
+      <img src={Logo} alt="AirCnC"/>
+
+      <div className="content">     
+      <Routes />      
+      </div>
+    </div>
+  );
+}
+
+export default App;
